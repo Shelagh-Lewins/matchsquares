867f095ca40ef0cb98cff9726333e2f4e0729db5
Shelagh Lewins <shelagh_lewins@hotmail.com>
2018-03-15 18:50:25 +0000

    added Donate button

--- imports/ui/pages/About/About.jsx
+++ imports/ui/pages/About/About.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+import DonateButton from '../../components/DonateButton/DonateButton.jsx';
 
 const About = () => (
 	<div>
 		<h1>About MatchSquares</h1>
 		<p>MatchSquares is an interactive puzzle game created by Shelagh Lewins.</p>
-		<p>Game design, code and artwork copyright &copy; Shelagh Lewins, 7 March 2018.</p>
+		<Row>
+			<Col xs={12} sm={6}>
+				<p>MatchSquares is free to play and there are no adverts. If you enjoy playing MatchSquares, perhaps you might make a small donation to help keep the server running and the developer supplied with coffee? It will make the developer very happy and give them a warm fuzzy feeling all day.</p>
+			</Col>
+			<Col xs={12} sm={6}>
+				<DonateButton />
+			</Col>
+		</Row>
+		<Row>
+			<Col xs={12} sm={12}>
+				<p>Game design, code and artwork copyright &copy; Shelagh Lewins, 7 March 2018.</p>
+			</Col>
+		</Row>
 	</div>
 );
 
--- imports/ui/pages/Home/Home.jsx
+++ imports/ui/pages/Home/Home.jsx
@@ -1,13 +1,27 @@
 
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+// import './Home.scss';
 
 // component that lists maps
 import MapsList from '../../components/MapsList/MapsList.jsx';
+import DonateButton from '../../components/DonateButton/DonateButton.jsx';
 
 const Home = ({ history }) => (
 	<div className="Home">
 		<h1>MatchSquares</h1>
-		<p>Tap tiles until they match the pattern displayed above the board in this simple puzzle game. Choose a board and start playing!</p>
+		<Row>
+			<Col xs={12} sm={6}>
+				<p>Tap tiles until they match the pattern displayed above the board in this simple puzzle game. Choose a board and start playing!</p>
+			</Col>
+			<Col xs={12} sm={6}>
+				<p>MatchSquares is free to play and there are no adverts. If you enjoy playing MatchSquares, perhaps you might make a small donation to help keep the server running and the developer supplied with coffee?</p>
+				<DonateButton
+					center={true}
+				/>
+			</Col>
+		</Row>
 		<MapsList
 			history={history}
 		/>
