a84ea7aba781bc35b53229c71f83c7d416e8dddd
Shelagh Lewins <shelagh_lewins@hotmail.com>
2018-03-17 15:32:37 +0000

    changed Home to a component so it can do stuff for a background

--- imports/ui/pages/Home/Home.jsx
+++ imports/ui/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
-
-import React from 'react';
+import React, { Component } from 'react';
 import { Row, Col } from 'react-bootstrap';
 
 // import './Home.scss';
@@ -8,25 +7,29 @@ import { Row, Col } from 'react-bootstrap';
 import MapsList from '../../components/MapsList/MapsList.jsx';
 import DonateButton from '../../components/DonateButton/DonateButton.jsx';
 
-const Home = ({ history }) => (
-	<div className="Home">
-		<h1>MatchSquares</h1>
-		<Row>
-			<Col xs={12} sm={6}>
-				<p>Tap tiles until they match the pattern displayed above the board in this simple puzzle game. Choose a board and start playing!</p>
-			</Col>
-			<Col xs={12} sm={6}>
-				<p>MatchSquares is free to play and there are no adverts. If you enjoy playing MatchSquares, perhaps you might make a small donation?</p>
-				<DonateButton
-					center={true}
+class Home extends Component {
+	render() {
+		return (
+			<div className="Home">
+				<h1>MatchSquares</h1>
+				<Row>
+					<Col xs={12} sm={6}>
+						<p>Tap tiles until they match the pattern displayed above the board in this simple puzzle game. Choose a board and start playing!</p>
+					</Col>
+					<Col xs={12} sm={6}>
+						<p>MatchSquares is free to play and there are no adverts. If you enjoy playing MatchSquares, perhaps you might make a small donation?</p>
+						<DonateButton
+							center={true}
+						/>
+					</Col>
+				</Row>
+				<MapsList
+					history={history}
 				/>
-			</Col>
-		</Row>
-		<MapsList
-			history={history}
-		/>
-	</div>
-);
+			</div>
+		);
+	}
+}
 
 export default Home;
 
