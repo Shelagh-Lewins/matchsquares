d16ca043123a3b0f5b55ce0af7b02521ea2610f9
Shelagh Lewins <shelagh_lewins@hotmail.com>
2018-03-28 17:53:19 +0100

    added rules to About page

--- imports/ui/pages/About/About.jsx
+++ imports/ui/pages/About/About.jsx
@@ -40,6 +40,30 @@ class About extends Component {
 								<p>MatchSquares is free to play and there are no adverts. If you enjoy playing MatchSquares, perhaps you might make a small donation to help keep the server running and the developer supplied with coffee? It will make the developer very happy and give them a warm fuzzy feeling all day.</p>
 							</div>
 						</Col>
+						<Col xs={12}>
+							<div className='box'>
+								<h2>How to play</h2>
+								<p>Tap tiles until they match the pattern displayed above the board.</p>
+								<p>Tapping a tile will change it to the next shape in the sequence:</p>
+								<p>triangle &gt; square &gt; pentagon &gt; hexagon</p>
+								<p>Neighbouring tiles with the same shape will also change!</p>
+							</div>
+						</Col>
+						<Col xs={12}>
+							<div className='box'>
+								<h2>Challenging boards</h2>
+								<p>If you play a Challenging board, there are three types of pattern.</p>
+								<dl className="rules">
+									<dt>Generated</dt>
+									<dd>The pattern has been constructed so that it can always be solved.</dd>
+									<dt>Random</dt>
+									<dd>The pattern is completely random. It may not be possible to solve this pattern.</dd>
+									<dt>ID</dt>
+									<dd>Every pattern is defined by a series of numbers and lower-case letters. Entering an ID lets you try the same pattern again, or share it with a friend to see if they can solve it.</dd>
+								</dl>
+								<p>I would love to know if there is a mathematical way to tell whether a random pattern can be solved. If you work this out, please let me know!</p>
+							</div>
+						</Col>
 					</Row>
 				</div>
 			</div>
